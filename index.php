2a7ce545e3b05e6f0a2dcafeb56e2021861a3d1c
Satarud3 <sataruddin28@gmail.com>
2026-04-14 08:46:24 +0800

    update v1

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = "budi";
+    $umur = 20;
+    $tinggi = 175.9;
+    $hobi = ["membaca", "berenang", "mengaji"];
+
+    echo "nama saya $nama, umur saya $umur, Tinggi saya $tinggi, hobi saya $hobi[2]";
+
+?>
