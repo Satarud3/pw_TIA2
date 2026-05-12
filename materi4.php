a27675158cd5be7dae5e7ac8399f4159ab941544
Satarud3 <sataruddin28@gmail.com>
2026-05-12 08:46:02 +0800

    Pertemuan 7

--- materi4.php
+++ materi4.php
@@ -25,7 +25,7 @@ if (isset($_POST["kirim"])) {
 
 ?>
 
-<table border="1" cellpading="15" cellspacing="0">
+<table border="1" cellpadding="12" cellspacing="0">
     <tr>
         <th>Username</th>
         <th>Password</th>
@@ -47,53 +47,48 @@ if (isset($_POST["kirim"])) {
         echo "</tr>";
     } 
 
-    function hapus($id){
-        if (isset($_GET['id'])) {
-            $id = $_GET['id'];
-            $query = "DELETE FROM user WHERE id_user = '$id'";
-            if (mysqli_query($koneksi, $query)) {
-                echo "Data berhasil dihapus";
-                header("Location: materi4.php");
-            } else {
-                echo "Error: " . $query . "<br>" . mysqli_error($koneksi);
-            }
+        if (isset($_GET['hapus'])) {
+        $id = $_GET['hapus'];
+        $query = "DELETE FROM user WHERE id_user = '$id'";
+        if (mysqli_query($koneksi, $query)) {
+            echo "Data berhasil dihapus";
+            header("Location: materi4.php");
+        } else {
+            echo "Error: " . $query . "<br>" . mysqli_error($koneksi);
         }
-    }
+        }
 
-    function edit ($id){
-        if (isset($_GET['id'])) {
-            $id = $_GET['id'];
-            $query = "SELECT * FROM user WHERE id_user = '$id'";
-            $result = mysqli_query($koneksi, $query);
-            if ($row = mysqli_fetch_assoc($result)) {
-                echo "<form method='POST'>";
-                echo "Username: <input type='text' name='username' value='" . $row['username'] . "'><br>";
-                echo "Password: <input type='password' name='password' value='" . $row['password'] . "'><br>";
-                echo "Nama: <input type='text' name='nama' value='" . $row['nama'] . "'><br>";
-                echo "Email: <input type='email' name='email' value='" . $row['email'] . "'><br>";
-                echo "<input type='submit' value='Update'>";
-                echo "</form>";
+    if (isset($_GET['edit'])) {
+        $id = $_GET['edit'];
+        $query = "SELECT * FROM user WHERE id_user = '$id'";
+        $result = mysqli_query($koneksi, $query);
+        if ($row = mysqli_fetch_assoc($result)) {
+            echo "<form method='POST'>";
+            echo "Username: <input type='text' name='username' value='" . $row['username'] . "'><br>";
+            echo "Password: <input type='password' name='password' value='" . $row['password'] . "'><br>";
+            echo "Nama: <input type='text' name='nama' value='" . $row['nama'] . "'><br>";
+            echo "Email: <input type='email' name='email' value='" . $row['email'] . "'><br>";
+            echo "<input type='submit' value='Update'>";
+            echo "</form>";
 
-                if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['nama']) && isset($_POST['email'])) {
-                    $username = $_POST['username'];
-                    $password = $_POST['password'];
-                    $nama = $_POST['nama'];
-                    $email = $_POST['email'];
+            if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['nama']) && isset($_POST['email'])) {
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                $nama = $_POST['nama'];
+                $email = $_POST['email'];
 
-                    $updateQuery = "UPDATE user SET username='$username', password='$password', nama='$nama', email='$email' WHERE id_user='$id'";
-                    if (mysqli_query($koneksi, $updateQuery)) {
-                        echo "Data berhasil diupdate";
-                        header("Location: materi4.php");
-                    } else {
-                        echo "Error: " . $updateQuery . "<br>" . mysqli_error($koneksi);
-                    }
+                $updateQuery = "UPDATE user SET username='$username', password='$password', nama='$nama', email='$email' WHERE id_user='$id'";
+                if (mysqli_query($koneksi, $updateQuery)) {
+                    echo "Data berhasil diupdate";
+                    header("Location: materi4.php");
+                } else {
+                    echo "Error: " . $updateQuery . "<br>" . mysqli_error($koneksi);
                 }
+            }
             } else {
                 echo "Data tidak ditemukan";
             }
-        }
-    }
-   
+    }   
     ?>
 
 </table>
